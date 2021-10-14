578075693d86e7f5967c0839e8fc8997481613c2
David Lyon <dauglyon@users.noreply.github.com>
2021-10-14 12:25:12 -0700

    Add colors story, remove default examples from storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 module.exports = {
   "stories": [
+    "../src/stories/intro.stories.mdx",
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
@@ -7,5 +8,5 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
 }
