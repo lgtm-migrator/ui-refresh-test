51f5642745acdd1a0fec4f7638eb40285e62af56
David Lyon <dauglyon@users.noreply.github.com>
2021-10-26 15:44:55 -0700

    better component structure for LeftNavBar

--- src/common/nav/LeftNavBar.tsx
+++ src/common/nav/LeftNavBar.tsx
@@ -1,3 +1,4 @@
+import { FC } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import classes from './LeftNavBar.module.scss';
 import { FontAwesomeIcon as FAIcon } from '@fortawesome/react-fontawesome';
@@ -13,39 +14,40 @@ import {
   IconDefinition,
 } from '@fortawesome/free-solid-svg-icons';
 
-const navItems: [string, string, IconDefinition][] = [
-  // [path, desc, icon]
-  ['/', 'Navigator', faCompass],
-  ['/orgs', 'Orgs', faUsers],
-  ['/catalog', 'Catalog', faBook],
-  ['/search', 'Search', faSearch],
-  ['/jobs', 'Jobs', faSuitcase],
-  ['/account', 'Account', faIdCard],
-  ['/feeds', 'Feeds', faBullhorn],
-  ['/count', 'Count', faExclamation],
-  ['/auth', 'Auth', faExclamation],
-];
-
 export default function Nav() {
-  const location = useLocation();
   return (
     <nav>
       <ul className={classes.nav_list}>
-        {navItems.map(([path, desc, icon]) => {
-          let itemClasses = classes.nav_item;
-          if (location.pathname === path) {
-            itemClasses += ` ${classes.active}`;
-          }
-          return (
-            <li className={itemClasses} key={path}>
-              <Link to={path}>
-                <FAIcon className={classes.nav_icon} icon={icon} />
-                <span className={classes.nav_desc}>{desc}</span>
-              </Link>
-            </li>
-          );
-        })}
+        <NavItem path="/" desc="Navigator" icon={faCompass} />
+        <NavItem path="/orgs" desc="Orgs" icon={faUsers} />
+        <NavItem path="/catalog" desc="Catalog" icon={faBook} />
+        <NavItem path="/search" desc="Search" icon={faSearch} />
+        <NavItem path="/jobs" desc="Jobs" icon={faSuitcase} />
+        <NavItem path="/account" desc="Account" icon={faIdCard} />
+        <NavItem path="/feeds" desc="Feeds" icon={faBullhorn} />
+        <NavItem path="/count" desc="Count" icon={faExclamation} />
+        <NavItem path="/auth" desc="Auth" icon={faExclamation} />
       </ul>
     </nav>
   );
 }
+
+const NavItem: FC<{ path: string; desc: string; icon: IconDefinition }> = ({
+  path,
+  desc,
+  icon,
+}) => {
+  const location = useLocation();
+  let itemClasses = classes.nav_item;
+  if (location.pathname === path) {
+    itemClasses += ` ${classes.active}`;
+  }
+  return (
+    <li className={itemClasses} key={path}>
+      <Link to={path}>
+        <FAIcon className={classes.nav_icon} icon={icon} />
+        <span className={classes.nav_desc}>{desc}</span>
+      </Link>
+    </li>
+  );
+};
