b8ed13ab2c5bf438a04dc90c89e76331a93bc72d
David Lyon <dauglyon@users.noreply.github.com>
2021-11-16 17:17:31 -0800

    Added select/dropdown, added topbar dropdowns

---
+++ src/common/components/Dropdown.tsx
@@ -0,0 +1,24 @@
+import { FC, ComponentPropsWithRef } from 'react';
+import { components } from 'react-select';
+import classes from './Dropdown.module.scss';
+import { Select } from './Select';
+
+const { DropdownIndicator } = components;
+
+type DropdownProps = ComponentPropsWithRef<typeof Select>;
+
+export const Dropdown: FC<DropdownProps> = (props) => {
+  const indicator = props.children;
+  return (
+    <Select
+      {...props}
+      value={null}
+      className={[classes['dropdown'], props.className].join(' ')}
+      components={{
+        DropdownIndicator: (props) => (
+          <DropdownIndicator {...props}>{indicator}</DropdownIndicator>
+        ),
+      }}
+    />
+  );
+};
--- src/features/layout/TopBar.tsx
+++ src/features/layout/TopBar.tsx
@@ -4,12 +4,24 @@ import classes from './TopBar.module.scss';
 import { FontAwesomeIcon as FAIcon } from '@fortawesome/react-fontawesome';
 import {
   faBars,
+  faEnvelope,
+  faFile,
   faFlask,
+  faIdCard,
+  faInfo,
+  faPlus,
+  faQuestion,
   faQuestionCircle,
+  faSearch,
+  faServer,
+  faSignOutAlt,
   faSortDown,
+  faSquare,
   faUser,
   faWrench,
 } from '@fortawesome/free-solid-svg-icons';
+import { Dropdown } from '../../common/components';
+import { useHistory } from 'react-router-dom';
 
 import logo from '../../common/assets/logo/46_square.png';
 
@@ -28,18 +40,140 @@ export default function TopBar() {
       <div className={classes.topbar_item}>
         <Enviroment />
       </div>
-      <div className={classes.login_menu}>
-        <FAIcon icon={faUser} />
-        <FAIcon icon={faSortDown} />
+      <div className={classes.topbar_item}>
+        <LoginMenu />
       </div>
     </header>
   );
 }
 
+const LoginMenu: FC = () => {
+  const history = useHistory();
+  return (
+    <div className={classes.login_menu}>
+      <Dropdown
+        horizontalMenuAlign="right"
+        options={[
+          {
+            options: [
+              {
+                value: '',
+                icon: undefined,
+                label: (
+                  <div>
+                    <div>First Last</div>
+                    <div className={classes.login_menu_username}>username</div>
+                  </div>
+                ),
+              },
+            ],
+          },
+          {
+            options: [
+              {
+                value: '#your_profile',
+                icon: <FAIcon icon={faUser} />,
+                label: 'Your Profile',
+              },
+              {
+                value: '#your_account',
+                icon: <FAIcon icon={faIdCard} />,
+                label: 'Your Account',
+              },
+            ],
+          },
+          {
+            options: [
+              {
+                value: '#sign_out',
+                icon: <FAIcon icon={faSignOutAlt} />,
+                label: 'Sign Out',
+              },
+            ],
+          },
+        ]}
+        onChange={(opt) => {
+          if (opt && 'value' in opt) {
+            history.push(opt?.value);
+          }
+        }}
+      >
+        <div className={classes.login_menu_button}>
+          <FAIcon className={classes.login_menu_icon} icon={faSquare} />
+          <FAIcon icon={faSortDown} />
+        </div>
+      </Dropdown>
+    </div>
+  );
+};
+
 const HamburgerMenu: FC = () => {
+  const history = useHistory();
   return (
     <div className={classes.hamburger_menu}>
-      <FAIcon icon={faBars} />
+      <Dropdown
+        options={[
+          {
+            options: [
+              {
+                value: '#narrative_interface',
+                icon: <FAIcon icon={faFile} />,
+                label: 'Narrative Interface',
+              },
+              {
+                value: '#new_narrative',
+                icon: <FAIcon icon={faPlus} />,
+                label: 'New Narrative',
+              },
+              {
+                value: '#jgi_search',
+                icon: <FAIcon icon={faSearch} />,
+                label: 'JGI Search',
+              },
+              {
+                value: '#biochem_search',
+                icon: <FAIcon icon={faSearch} />,
+                label: 'Biochem Search',
+              },
+            ],
+          },
+          {
+            options: [
+              {
+                value: '#kbase_services_status',
+                icon: <FAIcon icon={faServer} />,
+                label: 'KBase Services Status',
+              },
+            ],
+          },
+          {
+            options: [
+              {
+                value: '#about',
+                icon: <FAIcon icon={faInfo} />,
+                label: 'About',
+              },
+              {
+                value: '#contact_kbase',
+                icon: <FAIcon icon={faEnvelope} />,
+                label: 'Contact KBase',
+              },
+              {
+                value: '#support',
+                icon: <FAIcon icon={faQuestion} />,
+                label: 'Support',
+              },
+            ],
+          },
+        ]}
+        onChange={(opt) => {
+          if (opt && 'value' in opt) {
+            history.push(opt?.value);
+          }
+        }}
+      >
+        <FAIcon className={classes.hamburger_menu_icon} icon={faBars} />
+      </Dropdown>
     </div>
   );
 };
