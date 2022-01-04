6929b30d6a51e4238718b8f9159877fc654a4403
Dakota Blair <dblair@bnl.gov>
2022-01-04 11:08:56 -0500

    Change color classes to BEM convention.

--- src/common/components/Button.tsx
+++ src/common/components/Button.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent as FC } from 'react';
 import classes from './Button.module.scss';
 
-export const Button: FC<React.HTMLAttributes<HTMLButtonElement>> = (props) => {
+export const Button: FC<React.ComponentProps<'button'>> = (props) => {
   const className = [classes.button, props.className].join(' ');
   return <button className={className} type={undefined} {...props} />;
 };
--- src/stories/components/Button.stories.tsx
+++ src/stories/components/Button.stories.tsx
@@ -2,33 +2,20 @@ import { MouseEvent } from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Button } from '../../common/components';
+import classes from '../../common/components/Button.module.scss';
 
 export default {
   title: 'Components/Button',
   component: Button,
 } as ComponentMeta<typeof Button>;
 
-const Colors = [
-  'orange',
-  'yellow',
-  'green',
-  'grellow',
-  'blue',
-  'ocean-blue',
-  'teal',
-  'purple',
-  'frost-blue',
-  'red',
-  'grey',
-];
-
 const randomBackground = (evt: MouseEvent<HTMLButtonElement>) => {
-  console.log('Clicked!', evt); // eslint-disable-line no-console
-  const randomIndex = Math.floor(11 * Math.random());
-  const randomColor = `bg-${Colors[randomIndex]}`;
-  const classes = evt.currentTarget.classList;
-  classes.forEach((cls) => classes.remove(cls));
-  classes.add(randomColor);
+  const classNames = Object.keys(classes);
+  const randomIndex = Math.floor(classNames.length * Math.random());
+  const randomClass = classes[classNames[randomIndex]];
+  const currentClasses = evt.currentTarget.classList;
+  currentClasses.forEach((cls) => currentClasses.remove(cls));
+  currentClasses.add(randomClass);
 };
 
 const ButtonTemplate: ComponentStory<typeof Button> = (args) => (
