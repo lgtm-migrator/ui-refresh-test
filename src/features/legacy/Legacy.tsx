6ee17d553909904273ac95ecfc3788480a67c242
David Lyon <dauglyon@users.noreply.github.com>
2022-02-01 13:06:13 -0800

    add iframe height management, immediately set iframe src in update

--- src/features/legacy/Legacy.tsx
+++ src/features/legacy/Legacy.tsx
@@ -9,10 +9,14 @@ export default function Legacy() {
 
   const legacyContent = useRef<HTMLIFrameElement>(null);
 
+  // path state
   const initPath =
     legacyPart(history.location.pathname) + history.location.hash;
   const [initialLegacyPath, setInitialLegacyPath] = useState<string>(initPath);
   const [legacyPath, setLegacyPath] = useState<string>(initPath);
+
+  //height state
+  const [legacyHeight, setLegacyHeight] = useState<string>('500vh');
 
   useEffect(() => {
     // This effect compares the inner and outer URLS (app URL and iframe URL)
@@ -22,9 +26,14 @@ export default function Legacy() {
     // If both inner & outer URLs differ from state, the inner URL takes
     // precedence.
 
+    // This effect also manages the iframe height, as this needs to be updated
+    // manually.
+
     const checkInterval = setInterval(() => {
+      // sync urls
       const innerLocation = legacyContent.current?.contentWindow?.location;
       const outerLocation = window.location;
+      let pathChanged = false;
       if (innerLocation) {
         // ensures the iframe has mounted
         const inner = innerLocation.pathname + (innerLocation.hash ?? '');
@@ -32,26 +41,49 @@ export default function Legacy() {
           legacyPart(outerLocation.pathname) + (outerLocation.hash ?? '');
         if (inner !== legacyPath) {
           setLegacyPath(inner);
+          pathChanged = true;
           history.push(`/legacy${inner}`);
         } else if (outer !== legacyPath) {
           setLegacyPath(outer);
           setInitialLegacyPath(outer);
+          legacyContent.current.src = outer;
+          pathChanged = true;
         }
+      }
+
+      // set iframe height when the path changes or content exceeds the iframe
+      // this may be slow if the inner content changes height frequently
+      const iframeBody = legacyContent.current?.contentWindow?.document.body;
+      const checkheight = iframeBody?.scrollHeight.toString();
+      if (
+        legacyContent.current &&
+        checkheight &&
+        (pathChanged || checkheight !== legacyHeight)
+      ) {
+        const scrollY = window.scrollY; // save current scroll position
+        // reset iframe height, force reflow
+        legacyContent.current.height = '500vh';
+        const setHeight = iframeBody?.scrollHeight.toString() || checkheight;
+        legacyContent.current.height = setHeight;
+        setLegacyHeight(setHeight);
+        window.scrollTo(0, scrollY); // restore scroll position
       }
     }, 200);
     return () => {
       clearInterval(checkInterval);
     };
-  }, [history, legacyContent, legacyPath]);
+  }, [history, legacyContent, legacyPath, legacyHeight]);
 
   return (
     <section>
       <iframe
+        style={{ overflowY: 'hidden' }}
+        frameBorder="0"
         src={initialLegacyPath}
         ref={legacyContent}
         title="Legacy Content"
         width="100%"
-        height="1000vh"
+        height={legacyHeight}
       />
     </section>
   );
